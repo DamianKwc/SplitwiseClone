2660d52de682679ff990e3e313749048f7531391
DamianKwc <147318537+DamianKwc@users.noreply.github.com>
2023-12-19 17:39:09 +0100

    Add files via upload

---
+++ SplitwiseClone/src/test/java/Splitwise/Clone/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Splitwise.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
